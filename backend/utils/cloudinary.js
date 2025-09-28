c70690ff6b39538e370bf590bb539b7b5a035a8f
Wasana Karunasena <141900162+WasanaKarunasena@users.noreply.github.com>
2025-09-28 18:26:21 +0530

    Add files via upload

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dtwy93e7r',
+  api_key: '441843641641975',
+  api_secret: '4ds0HLTE24-gVnl-VRjbiacCmSM',
+});
+
+module.exports = cloudinary;
